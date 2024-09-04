b9de59b2f11ddd762cd85062133e95f311f9b181
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2024-09-05 02:04:22 +0600

    gradle added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures{
+        viewBinding ; true //addeed by istiaq
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
