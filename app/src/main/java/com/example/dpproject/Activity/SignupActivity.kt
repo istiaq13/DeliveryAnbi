9c04d88db8a5740bcbe21809cf25bab2ff192fb5
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2024-09-05 11:32:50 +0600

    firebase database connected

--- app/src/main/java/com/example/dpproject/Activity/SignupActivity.kt
+++ app/src/main/java/com/example/dpproject/Activity/SignupActivity.kt
@@ -6,10 +6,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dpproject.R
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var email : String
+    private lateinit var password : String
+    private lateinit var username : String
+    private lateinit var Database : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //initialize Firebase Authentication
+        auth = Firebase.auth
+        //initialize Firebase Database
+        Database = Firebase.database.reference
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
