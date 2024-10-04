030c37cd11cd43af93fa3a0b38e8b7e451d1ca7c
Md. Abid Hasan <emonff89@gmail.com>
2024-10-04 17:24:35 +0600

    email authentication complete

--- app/src/main/java/com/example/dpproject/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/dpproject/Activity/LoginActivity.kt
@@ -194,42 +194,31 @@ class LoginActivity : AppCompatActivity() {
         alertDialog.show()
     }
 
-    private fun loginUser(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                // Redirect to DashboardActivity after successful login
-                val intent = Intent(this, DashboardActivity::class.java)
-                startActivity(intent)
-                finish() // Optionally, close the LoginActivity
-            } else {
-                showToast("Login Failed. Check your email or password.")
-            }
-        }
-    }
+
 
 //    login after authentication
-//private fun loginUser(email: String, password: String) {
-//    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-//        if (task.isSuccessful) {
-//            // Redirect to DashboardActivity after successful login
-//           val verification = auth.currentUser?.isEmailVerified
-//            if (verification==true){
-//
-//            val intent = Intent(this, DashboardActivity::class.java)
-//            startActivity(intent)
-//            finish() // Optionally, close the LoginActivity
-//        }
-//            else
-//            {
-//                showToast("Please verify your email")
-//            }
-//        }
-//
-//            else {
-//            showToast("Login Failed. Check your email or password.")
-//        }
-//    }
-//}
+private fun loginUser(email: String, password: String) {
+    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            // Redirect to DashboardActivity after successful login
+           val verification = auth.currentUser?.isEmailVerified
+            if (verification==true){
+
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+            finish() // Optionally, close the LoginActivity
+        }
+            else
+            {
+                showToast("Please verify your email")
+            }
+        }
+
+            else {
+            showToast("Login Failed. Check your email or password.")
+        }
+    }
+}
 
     private fun sendPasswordByEmail(email: String) {
         // Fetch password from Firestore (or your database)
